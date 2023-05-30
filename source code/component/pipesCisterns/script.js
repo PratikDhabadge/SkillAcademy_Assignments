0b70c2f2071ff6d62f5445ec4af6b472105fb484
Pratik Dhabadge <120300185+PratikDhabadge@users.noreply.github.com>
2023-05-30 21:48:22 +0530

    Add files via upload

---
+++ source code/component/pipesCisterns/script.js
@@ -0,0 +1,92 @@
+const questions=[
+    {
+        question: 'Pipes A and B can fill a tank in 5 and 6 hours respectively. Pipe C can empty it in 12 hours. If all the three pipes are opened together, then the tank will be filled in',
+        answers: [
+            { text : '60/17', correct: true},
+            { text : '22/17', correct: false},
+            { text : '30/11', correct: false},
+            { text : '9/2', correct: false},
+        ]
+    },
+    {
+        question: 'A pump can fill a tank with water in 2 hours. Because of a leak, it took 2 hours to fill the tank. The leak can drain all the water of the tank in',
+        answers: [
+            { text : '8 hours', correct: false},
+            { text : '2 hours', correct: false},
+            { text : '14 hours', correct: true},
+            { text : '12 hours', correct: false},
+        ]
+    },
+    {
+        question: 'Two pipes A and B can fill a cistern in 37 minutes and 45 minutes respectively. Both pipes are opened. The cistern will be filled in just half an hour, if the B is turned off after',
+        answers: [
+            { text : '4 min', correct: false},
+            { text : '9 min', correct: true},
+            { text : '6 min', correct: false},
+            { text : '15 min', correct: false},
+        ]
+    },
+    {
+        question: 'A tank is filled in 5 hours by three pipes A, B and C. The pipe C is twice as fast as B and B is twice as fast as A. How much time will pipe A alone take to fill the tank',
+        answers: [
+            { text : '20 hours', correct: false},
+            { text : '22 hours', correct: false},
+            { text : '35 hours', correct: true},
+            { text : 'None of these', correct: false},
+        ]
+    },
+    {
+        question: 'Two pipes A and B can fill a tank in 20 and 30 minutes respectively. If both the pipes are used together, then how long will it take to fill the tank?',
+        answers: [
+            { text : '14 min', correct: false},
+            { text : '12 min', correct: true},
+            { text : '5 min', correct: false},
+            { text : '9 min', correct: false},
+        ]
+    },
+    {
+        question: 'One pipe can fill a tank three times as fast as another pipe. If together the two pipes can fill the tank in 36 minutes, then the slower pipe alone will be able to fill the tank in:',
+        answers: [
+            { text : '141 min', correct: false},
+            { text : '211 min', correct: false},
+            { text : '144 min', correct: true},
+            { text : '99 min', correct: false},
+        ]
+    },
+    {
+        question: 'A tap can fill a tank in 6 hours. After half the tank is filled, three more similar taps are opened. What is the total time taken to fill the tank completely?',
+        answers: [
+            { text : '3 hrs 45 min', correct: true},
+            { text : '3 hrs', correct: false},
+            { text : '2 hrs 25 min', correct: false},
+            { text : '1 hrs 45 min', correct: false},
+        ]
+    },
+    {
+        question: 'Three taps A, B and C can fill a tank in 12, 15 and 20 hours respectively. If A is open all the time and B and C are open for one hour each alternately, the tank will be full in:',
+        answers: [
+            { text : '6 hours', correct: false},
+            { text : '2 hours', correct: false},
+            { text : '7 hours', correct: true},
+            { text : '5 hours', correct: false},
+        ]
+    },
+    {
+        question: 'Three pipes A, B and C can fill a tank in 6 hours. After working at it together for 2 hours, C is closed and A and B can fill the remaining part in 7 hours. The number of hours taken by C alone to fill the tank is:',
+        answers: [
+            { text : '12', correct: false},
+            { text : '22', correct: false},
+            { text : '14', correct: true},
+            { text : '19', correct: false},
+        ]
+    },
+    {
+        question: 'Two pipes A and B can fill a tank in 15 minutes and 20 minutes respectively. Both the pipes are opened together but after 4 minutes, pipe A is turned off. What is the total time required to fill the tank?',
+        answers: [
+            { text : '10 min 20 sec', correct: false},
+            { text : '15 min 30 sec', correct: false},
+            { text : '12 min 10 sec', correct: false},
+            { text : '14 min 40 sec', correct: true},
+        ]
+    },
+]
---
+++ source code/component/probability/script.js
@@ -0,0 +1,92 @@
+const questions=[
+    {
+        question: 'Tickets numbered 1 to 20 are mixed up and then a ticket is drawn at random. What is the probability that the ticket drawn has a number which is a multiple of 3 or 5?',
+        answers: [
+            { text : '1/2', correct: false},
+            { text : '2/5', correct: false},
+            { text : '3/11', correct: false},
+            { text : '9/20', correct: true},
+        ]
+    },
+    {
+        question: 'A bag contains 2 red, 3 green and 2 blue balls. Two balls are drawn at random. What is the probability that none of the balls drawn is blue?',
+        answers: [
+            { text : '11/2', correct: false},
+            { text : '10/21', correct: true},
+            { text : '2/7', correct: false},
+            { text : '5/11', correct: false},
+        ]
+    },
+    {
+        question: 'In a box, there are 8 red, 7 blue and 6 green balls. One ball is picked up randomly. What is the probability that it is neither red nor green?',
+        answers: [
+            { text : '8/5', correct: false},
+            { text : '1/3', correct: true},
+            { text : '15/21', correct: false},
+            { text : '11/3', correct: false},
+        ]
+    },
+    {
+        question: 'What is the probability of getting a sum 9 from two throws of a dice?',
+        answers: [
+            { text : '1/4', correct: false},
+            { text : '1/9', correct: true},
+            { text : '1/7', correct: false},
+            { text : '1/6', correct: false},
+        ]
+    },
+    {
+        question: 'Three unbiased coins are tossed. What is the probability of getting at most two heads?',
+        answers: [
+            { text : '3/4', correct: false},
+            { text : '7/8', correct: true},
+            { text : '1/2', correct: false},
+            { text : '4/3', correct: false},
+        ]
+    },
+    {
+        question: 'Two dice are thrown simultaneously. What is the probability of getting two numbers whose product is even?',
+        answers: [
+            { text : '1/2', correct: false},
+            { text : '3/4', correct: false},
+            { text : '5/4', correct: true},
+            { text : '4/3', correct: false},
+        ]
+    },
+    {
+        question: 'In a class, there are 15 boys and 10 girls. Three students are selected at random. The probability that 1 girl and 2 boys are selected, is:',
+        answers: [
+            { text : '11/26', correct: false},
+            { text : '21/46', correct: true},
+            { text : '23/17', correct: false},
+            { text : '15/11', correct: false},
+        ]
+    },
+    {
+        question: 'In a lottery, there are 10 prizes and 25 blanks. A lottery is drawn at random. What is the probability of getting a prize?',
+        answers: [
+            { text : '6/4', correct: false},
+            { text : '2/7', correct: true},
+            { text : '3/7', correct: false},
+            { text : '5/1', correct: false},
+        ]
+    },
+    {
+        question: 'From a pack of 52 cards, two cards are drawn together at random. What is the probability of both the cards being kings?',
+        answers: [
+            { text : '66/14', correct: false},
+            { text : '1/221', correct: true},
+            { text : '35/57', correct: false},
+            { text : '25/57', correct: false},
+        ]
+    },
+    {
+        question: 'Two dice are tossed. The probability that the total score is a prime number is:',
+        answers: [
+            { text : '3/7', correct: false},
+            { text : '6/4', correct: false},
+            { text : '5/12', correct: true},
+            { text : '7/9', correct: false},
+        ]
+    },
+]
